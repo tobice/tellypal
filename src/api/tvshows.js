94a9274deaff70d89876292b5b1d9e1acfa2c683
Tobiáš Potoček <tobiaspotocek@gmail.com>
2014-10-06 01:30:54 +0200

    added method for making urls

--- src/api/tvshows.js
+++ src/api/tvshows.js
@@ -1,6 +1,7 @@
 var request = require('superagent');
 var xmlHelpers = require('../lib/xmlHelpers');
 var _ = require('lodash');
+var url = require('url');
 
 var TVDB_API_KEY = '0A1C6FE6954B01FC';
 var TVDB_API_URL = 'http://thetvdb.com/api/';
@@ -8,7 +9,7 @@ var TVDB_API_URL = 'http://thetvdb.com/api/';
 var tvshows = {
 
     getSeries: function (seriesname) {
-        var url = TVDB_API_URL + 'GetSeries.php?seriesname=' + seriesname;
+        var url = this.makeUrl('GetSeries', {seriesname: seriesname});
         return request(url).promise()
             .then(xmlHelpers.parseXMLResponse)
             .then(function (json) {
@@ -17,7 +18,12 @@ var tvshows = {
             .then(xmlHelpers.cleanObjects);
     },
 
-
+    makeUrl: function (action, params) {
+        var tvdb = url.parse(TVDB_API_URL);
+        tvdb.pathname += action + '.php';
+        tvdb.query = params;
+        return url.format(tvdb);
+    }
 };
 
 module.exports = tvshows;
