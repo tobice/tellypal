f00106b1b7b8bb2ae7f7db5cfe45021988f4aeeb
Tobiáš Potoček <tobiaspotocek@gmail.com>
2014-10-17 02:02:31 +0200

    improved tvshow thumbnail

--- src/components/partials/SeriesThumbnail.jsx
+++ src/components/partials/SeriesThumbnail.jsx
@@ -4,14 +4,19 @@ var Banner = require('./Banner.jsx');
 
 var SeriesThumbnail = React.createClass({
 
+    getYear: function () {
+        var firstAired = this.props.series.FirstAired;
+        return firstAired ? firstAired.replace(/(\d+)-(\d+)-(\d+)/, "$1") : '(unknown year)';
+    },
+
     render: function () {
         var series = this.props.series;
         return (
             <div className="thumbnail">
                 <Banner seriesid={series.id} type="poster" />
                 <div className="caption">
-                    <h4>{series.SeriesName}</h4>
-                    <p>2000</p>
+                    <h4><a href="#">{series.SeriesName}</a></h4>
+                    <p className="text-muted">{this.getYear()}</p>
                 </div>
             </div>
         )
