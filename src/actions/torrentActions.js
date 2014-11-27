c20f025c13ea751b2bb077929377a923a888ee08
Tobiáš Potoček <tobiaspotocek@gmail.com>
2014-11-27 02:24:51 +0100

    detecting finished torrents on the server side (and sending appropriate notification)

--- src/actions/torrentActions.js
+++ src/actions/torrentActions.js
@@ -10,37 +10,6 @@ var torrentActions = {
 
     updateUi: function (flux, uiState) {
         flux.dispatch('TORRENTUI_UPDATE_STATE', uiState);
-
-        //var host = window.document.location.host.replace(/:.*/, '');
-        /*
-        var ws = new WebSocket('ws://' + host + ':3001'); // TODO: ports from config
-        var torrentUIStore = flux.getStore(TORRENTUI_STORE);
-        ws.onopen = function () {
-            debug('Opened WebSocket connection for updating Torrent client UI');
-        };
-        ws.onclose = function () {
-            debug('Closed WebSocket connection for updating Torrent client UI');
-            flux.notify('Lost connection!', 'Connection to the TellyPal server has been lost. Refresh to reconnect.', NotificationStore.DANGER);
-        };
-        ws.onmessage = function (event) {
-            var state = JSON.parse(event.data);
-            var torrents = state.torrents;
-
-            // Compare new state with the old one and find if any torrent has
-            // finished. If so, display user notification.
-            for (var hash in torrents) {
-                if (torrents.hasOwnProperty(hash)) {
-                    var torrent = torrentUIStore.getTorrent(hash);
-                    if (torrent && torrent.progress != 100 && torrents[hash].progress == 100) {
-                        var message = 'Torrent ' + torrent.name + ' has been successfully downloaded.';
-                        flux.notify('Torrent finished!', message, NotificationStore.SUCCESS);
-                    }
-                }
-            }
-
-            flux.dispatch('TORRENTUI_UPDATE_STATE', JSON.parse(event.data));
-        };
-        */
     }
 };
 
--- src/daemon.js
+++ src/daemon.js
@@ -3,18 +3,37 @@ var TorrentApi = require('./api/TorrentsApi');
 var socket = require('./utils/socketServer');
 var notifications = require('./libs/notifications');
 var torrentApi = new TorrentApi();
+var consts = require('./consts');
+
+var oldUiState;
 
 var daemon = {
 
     updateUi: function () {
-        if (socket.isSomeoneConnected()) {
-            torrentApi.updateUi().then(function (uiState) {
-                socket.send('updateUi', uiState);
-            });
-        }
+        torrentApi.updateUi().then(function (uiState) {
+            // Compare the new state with the old one and find if any
+            // torrent has finished. If so, display user notification.
+            if (oldUiState) {
+                var torrents = uiState.torrents;
+                for (var hash in torrents) {
+                    if (torrents.hasOwnProperty(hash)) {
+                        var torrent = oldUiState.torrents[hash];
+                        if (torrent && torrent.progress != 100 && torrents[hash].progress == 100) {
+                            var message = 'Torrent ' + torrent.name + ' has been successfully downloaded.';
+                            notifications.notify('Torrent finished!', message, consts.SUCCESS);
+                        }
+                    }
+                }
+            }
+
+            oldUiState = uiState;
+            socket.send('updateUi', uiState);
+        });
     },
 
     flushNotifications: function () {
+        // I have to be sure that there is some one to whom I can show the
+        // notifications (otherwise I will just wait until somebody connects)
         if (socket.isSomeoneConnected()) {
             _.each(notifications.flushNew(), function (notification) {
                 socket.send('notify', notification);
