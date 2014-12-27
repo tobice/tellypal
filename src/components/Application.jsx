c30528c93e2a4b9ae1119dab45bde35d8e10a9b0
Tobiáš Potoček <tobiaspotocek@gmail.com>
2014-12-27 15:42:15 +0100

    added contributions to the app footer

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -33,6 +33,16 @@ var Application = React.createClass({
                         <div className="row">
                             <div className="col-lg-12">
                                 <RouteHandler />
+
+                                <br />
+                                <hr />
+                                <p className="text-muted text-center">
+                                    Powered by{' '}
+                                    <a href="http://thetvdb.com/" target="_blank">The TVDB</a>,{' '}
+                                    <a href="http://torrentz.com/" target="_blank">Torrentz</a> and{' '}
+                                    <a href="http://deluge-torrent.org/" target="_blank">Deluge BitTorrent Client</a>
+                                </p>
+                                <br /><br />
                             </div>
                         </div>
                     </div>
