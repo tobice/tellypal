67de25f3e19582ddc2937fbbc8bf72f46cf51436
Tobiáš Potoček <tobiaspotocek@gmail.com>
2014-12-27 15:07:40 +0100

    added deluge-client dependency

--- src/libs/deluge.js
+++ src/libs/deluge.js
@@ -1,4 +1,4 @@
-var DelugeClient = require('./../../../deluge-client');
+var DelugeClient = require('deluge-client');
 var config = require('../../config');
 
 // Instantiate new connection to deluge
